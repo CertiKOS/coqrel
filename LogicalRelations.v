1afdd0be0b91af4a734ebef302a550decd7b8e75
Jeremie Koenig <jeremie.koenig@yale.edu>
2017-03-14 04:31:14 -0400

    Move some definitions to the new RDestruct.v

--- LogicalRelations.v
+++ LogicalRelations.v
@@ -2,6 +2,7 @@ Require Export RelDefinitions.
 Require Export RelOperators.
 Require Export Relators.
 Require Export Monotonicity.
+Require Export RDestruct.
 Require Export MorphismsInterop.
 Require Export Transport.
 Require Export PreOrderTactic.
