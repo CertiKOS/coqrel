46b4923c15d8527b03b86351692719b844219fec
Jeremie Koenig <jeremie.koenig@yale.edu>
2017-03-14 04:21:44 -0400

    Remember equations for destructed terms

--- RelDefinitions.v
+++ RelDefinitions.v
@@ -293,6 +293,54 @@ Ltac default_rdestruct :=
 Hint Extern 100 (RDestruct _ _) =>
   default_rdestruct : typeclass_instances.
 
+(** Sometimes we want to remember what the destructed terms were.
+  We use the following relation. *)
+
+Definition rdestruct_result {A B} m n (Q: rel A B) :=
+  fun x y => m = x /\ n = y -> Q x y.
+
+(* The default behavior is to simply discard the equations on the next
+  step. However, if the user wants to keep them, they can declare an
+  [rdestruct_remember] marker hypothesis to that end. *)
+
+Lemma rdestruct_forget_rintro {A B} m n (Q: rel A B) x y:
+  RIntro (Q x y) (rdestruct_result m n Q) x y.
+Proof.
+  firstorder.
+Qed.
+
+Lemma rdestruct_remember_rintro {A B} m n (Q: rel A B) x y:
+  RIntro (m = x -> n = y -> Q x y) (rdestruct_result m n Q) x y.
+Proof.
+  firstorder.
+Qed.
+
+(** You can use the [rdestruct_remember] tactic to declare that
+  hypothesis. *)
+
+CoInductive rdestruct_remember := rdestruct_remember_intro.
+
+Ltac rdestruct_remember :=
+  lazymatch goal with
+    | _ : rdestruct_remember |- _ =>
+      idtac
+    | _ =>
+      let Hrdestruct := fresh "Hrdestruct" in
+      pose proof rdestruct_remember_intro as Hrdestruct
+  end.
+
+(** The following hook switches between [rdestruct_forget] and
+  [rdestruct_remember] as a result. *)
+
+Ltac rdestruct_result_rintro :=
+  lazymatch goal with
+    | _ : rdestruct_remember |- _ => eapply rdestruct_remember_rintro
+    | _ => eapply rdestruct_forget_rintro
+  end.
+
+Hint Extern 100 (RIntro _ (rdestruct_result _ _ _) _ _) =>
+  rdestruct_result_rintro : typeclass_instances.
+
 (** To use [RDestruct] instances to reduce a goal involving pattern
   matching [G] := [_ (match m with _ end) (match n with _ end)], we
   need to establish that [m] and [n] are related by some relation [R],
@@ -304,10 +352,10 @@ Hint Extern 100 (RDestruct _ _) =>
   push a [delayed rauto] into a dead end. Thankfully, now we can use
   the [RAuto] typeclass to solve the [?R m n] subgoal in one swoop. *)
 
-Lemma rdestruct_rstep {A B} m n (R: rel A B) P Q:
+Lemma rdestruct_rstep {A B} m n (R: rel A B) P (Q: rel _ _):
   RAuto (R m n) ->
   RDestruct R P ->
-  P Q ->
+  P (rdestruct_result m n Q) ->
   Q m n.
 Proof.
   intros Hmn HR H.
