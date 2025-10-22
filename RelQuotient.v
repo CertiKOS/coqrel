d0526a659b0f5dfe7eac5bc753e5ed86900cd542
Jeremie Koenig <jeremie.koenig@yale.edu>
2025-10-21 20:06:03 -0400

    RelQuotient: generalize to heterogeneous relations

--- RelQuotient.v
+++ RelQuotient.v
@@ -29,25 +29,25 @@ Require Import ProofIrrelevance.
   Elements of [quotient R] as defined below are essentially the
   preimage sets induced by [R]. When [R] is an equivalence relation,
   this will correspond to the usual definition.
-  However, the construction will be meaningful in other cases.
+  However, the construction is meaningful in other cases.
   For example, when [R] is a preorder the construction gives the
   associated partial order. *)
 
-Record quotient {A} {R : relation A} :=
+Record quotient {A B} {R : rel A B} :=
   {
     in_eqcl : A -> Prop;
-    in_eqcl_wf : exists a, forall x, in_eqcl x <-> R x a;
+    in_eqcl_wf : exists b, forall a, in_eqcl a <-> R a b;
   }.
 
-Arguments quotient {A} R.
+Arguments quotient {A B} R.
 
 Local Obligation Tactic := solve [rauto | firstorder].
 
 (** Obtaining the equivalence class associated with
   an element of the base type is straightforward. *)
 
-Program Definition eqcl {A} (R : relation A) (a : A) : quotient R :=
-  {| in_eqcl u := R u a |}.
+Program Definition eqcl {A B} (R : rel A B) (b : B) : quotient R :=
+  {| in_eqcl a := R a b |}.
 
 Global Instance eqcl_params :
   Params (@eqcl) 1 := {}.
@@ -57,20 +57,20 @@ Global Instance eqcl_params :
 (** The preimage sets used to define the [quotient] type can be
   ordered by inclusion. *)
 
-Definition qle {A} {R : relation A} : relation (quotient R) :=
-  fun x y => forall u, in_eqcl x u -> in_eqcl y u.
+Definition qle {A B} {R : rel A B} : relation (quotient R) :=
+  fun x y => forall a, in_eqcl x a -> in_eqcl y a.
 
 (** The induced ordering is always a partial order, no matter what
   properties [R] may or may not have. *)
 
-Global Instance eqcl_le_preo {A} (R : relation A) :
-  PreOrder (@qle A R).
+Global Instance eqcl_le_preo {A B} (R : rel A B) :
+  PreOrder (@qle A B R).
 Proof.
   firstorder.
 Qed.
 
-Global Instance eqcl_le_po {A} (R : relation A) :
-  PartialOrder eq (@qle A R).
+Global Instance eqcl_le_po {A B} (R : rel A B) :
+  PartialOrder eq (@qle A B R).
 Proof.
   intros x y. cbn.
   split.
@@ -107,23 +107,23 @@ Qed.
   Below I attempt to formalize the corresponding properties
   in a fine-grained manner. *)
 
-Section PROPERTIES.
+(** Every element in the quotient set is obtained as the equivalence
+  class of an element of [A]. *)
+
+Lemma eqcl_surjective {A B} (R : rel A B) (x : quotient R) :
+  exists a, x = eqcl R a.
+Proof.
+  destruct (in_eqcl_wf x) as [a Ha]. exists a.
+  apply antisymmetry; firstorder.
+Qed.
+
+Section EQCL_PROPERTIES.
   Context {A} (R : relation A).
-
-  (** Every element in the quotient set is obtained as the equivalence
-    class of an element of [A]. *)
-
-  Lemma eqcl_surjective (x : quotient R) :
-    exists a, x = eqcl R a.
-  Proof.
-    destruct (in_eqcl_wf x) as [a Ha]. exists a.
-    apply antisymmetry; firstorder.
-  Qed.
 
   (** If [R] is transitive, then the principal downsets associated
     with related elements are ordered accordingly. *)
 
-  Global Instance eqcl_of_le `{HR : !Transitive R} :
+  Global Instance eqcl_le `{HR : !Transitive R} :
     Monotonic (eqcl R) (R ++> qle).
   Proof.
     intros u v Huv a. cbn. intro. rauto.
@@ -131,18 +131,19 @@ Section PROPERTIES.
 
   (** If [R] is a PER, then related elements become equal. *)
 
-  Global Instance eqcl_of_eq `{HR : !PER R} :
+  Global Instance eqcl_eq `{HR : !PER R} :
     Monotonic (eqcl R) (R ++> eq).
   Proof.
     intros u v Huv.
     apply antisymmetry; rauto.
   Qed.
 
-  (** If [R] is a reflexive, only related elements can become ordered. *)
+  (** If [R] is reflexive, only related elements can become ordered. *)
 
   Theorem eqcl_reflection `{HR : !Reflexive R} :
     forall x y, qle (eqcl R x) (eqcl R y) -> R x y.
   Proof.
     firstorder.
   Qed.
-End PROPERTIES.
+End EQCL_PROPERTIES.
+
